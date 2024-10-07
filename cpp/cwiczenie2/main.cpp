54e7933ff5b9b2069eeb34484e4122befcd4c06e
Piotrus2019PL <student@student.machine>
2024-10-07 11:08:33 +0000

    studentmachine send

--- cpp/cwiczenie2/main.cpp
+++ cpp/cwiczenie2/main.cpp
@@ -5,17 +5,24 @@ using namespace std;
 
 int main(){
 	vector<int> numbers;
-	numbers.push_back(4);
-	numbers.push_back(5);
-	numbers.push_back(2);
-	numbers.push_back(3);
-	
+	cout<<"ile liczb?";
+	int h=0;
+	cin>>h;	
 
-	for(int x=0;x<numbers.size(); x++){
-		for(int y=0;y<numbers.size(); y++){
-		swap(numbers[x], numbers[y]);
+
+	for(int m=0; m<=h-1; m++){
+		cout<<"podaj lb";
+		int n=0;
+		cin>>n;
+		numbers.push_back(n);
+		}
+
+	for(int x=0;x<numbers.size()-1; x++){
+		for(int y=0;y<numbers.size()-1; y++){
+			if(numbers[y]>numbers[y+1]){
+		swap(numbers[x], numbers[y+1]);
 	}
-
+}
 }
 for(int v=0; v<numbers.size(); v++){
 	cout<<numbers[v]<<'\n';
--- cpp/szablon/main.cpp
+++ cpp/szablon/main.cpp
@@ -3,6 +3,36 @@
 using namespace std;
 
 int main(){
-	cout<<"witaj swiecie"<<endl;
+	char pl[5][5];
+	for(int i = 0; i<5; i++){
+		for(int j=0; j<5; j++){
+		pl[i][j]='-';
+		}
+	}
+
+	int stX = 2;
+	int stY = 2;
+
+
+	while(0==0){
+		for(int i = 0; i<5; i++){
+			for(int j = 0; j<5; j++){
+			cout<<pl[i][j]<<" ";
+			}
+		cout<<endl;
+		}
+	int x,y;
+	cout<<"Gdzie strzelasz (x y): ";
+	cin>>x>>y;
+	if (x == stX && y == stY){
+		cout<<"Trafiony zatopiony!"<<endl;
+		break;
+	}else{
+		cout<<"Pudło!"<<endl;
+		pl[x][y] = '1';
+		}
+	}
+	
 	return 0;
 }
+	
